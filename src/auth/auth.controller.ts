d165d10efb3ac8be230b0ec4045485461dd2b2d9
imimran <engr.aih@gmail.com>
2021-02-16 18:11:14 +0600

    Add signup validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
+import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Task } from 'src/tasks/task.entity';
 
 @Controller('auth')
 export class AuthController {
+    constructor(
+        private authService: AuthService
+    ){}
 
     @Post('/signup')
-    signUp(@Body() authDto: AuthDto){
+    signUp(@Body(ValidationPipe) authDto: AuthDto): Promise<void>{
         console.log(authDto);
+        return this.authService.signUp(authDto)
         
     }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, MaxLength, MinLength } from "class-validator";
+
 export class AuthDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(5)
+    @MaxLength(20)
     password: string;
 }
